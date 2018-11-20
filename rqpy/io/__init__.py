ba85c686cddaaf1e33785439c24cdea4d8d95d1b
Samuel Watkins <samwatkins@galba.stanford.edu>
2018-11-20 09:30:37 -0800

    add check for scdmsPyTools

--- rqpy/io/__init__.py
+++ rqpy/io/__init__.py
@@ -1,2 +1,17 @@
-from . import io
-from .io import *
+from importlib.util import find_spec
+import sys
+
+package_req = 'scdmsPyTools'
+
+spec = find_spec(package_req)
+
+if spec is None:
+    print("scdmsPyTools is not installed, cannot import the io submodule")
+else:
+    from . import io
+    from .io import *
+
+del find_spec
+del sys
+del package_req
+del spec
--- rqpy/process/__init__.py
+++ rqpy/process/__init__.py
@@ -1,2 +1,17 @@
-from . import rq
-from .rq import *
+from importlib.util import find_spec
+import sys
+
+package_req = 'scdmsPyTools'
+
+spec = find_spec(package_req)
+
+if spec is None:
+    print("scdmsPyTools is not installed, cannot import the process submodule")
+else:
+    from . import rq
+    from .rq import *
+
+del find_spec
+del sys
+del package_req
+del spec
