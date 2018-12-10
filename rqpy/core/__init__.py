3b19b522a37e28ea3a0a35310cade1c0efd44555
cwfink <cwfink@berkeley.edu>
2018-12-10 14:03:51 -0800

    added docstring to scale_saturated_energy() in _rrq.py

--- rqpy/core/__init__.py
+++ rqpy/core/__init__.py
@@ -1,4 +1,4 @@
 from ._cut import *
 from ._pulse import *
 from ._fitting import *
-from ._rrq.py import *
+from ._rrq import *
--- rqpy/core/_rrq.py
+++ rqpy/core/_rrq.py
@@ -7,13 +7,32 @@ __all__ = ["scale_saturated_energy"]
 
 def scale_saturated_energy(vals, fitparams):
     """
-    Thing to do stuff
+    Function to convert saturated measured enrgy into 
+    true energy
+    
+    Parameters
+    ----------
+    vals: array
+        Array of measured energies to be converted to true energies
+    fitparams: list
+        List containing the best fit parameters from the fit_saturation() 
+        fuction. fitparams[0] should correspond to the optimum parameters
+        and fitparams[1] should be the covariance matrix from the fit
+        
+    Returns
+    -------
+    energy_true: array
+        Array of saturation corrected energies
+    errors: array
+        Array of uncertainties for each value in energy_true
+        
     """
+    
     params = fitparams[0]
     cov = fitparams[1]
     
     energy_true = utils.invert_saturation_func(vals, *params)
-    errors = utils.prop_sat_err(vals,popt,pcov)
+    errors = utils.prop_sat_err(vals, params, cov)
     
     return energy_true, errors
     
