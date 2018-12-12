661f4785d08b8ba90ba0a8648ecfcc193fc26f7c
Samuel Watkins <35979561+slwatkins@users.noreply.github.com>
2018-12-12 12:06:20 -0800

    update documentation, remove imports
    documentation should match conventions, and most of the imports were unnecessary

--- rqpy/core/_rrq.py
+++ rqpy/core/_rrq.py
@@ -1,29 +1,26 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from rqpy import plotting, utils
-import rqpy as rp
+from rqpy import utils
 
 __all__ = ["scale_saturated_energy"]
 
 def scale_saturated_energy(vals, fitparams):
     """
-    Function to convert saturated measured enrgy into 
+    Function to convert saturated measured energy into 
     true energy
     
     Parameters
     ----------
-    vals: array
+    vals : ndarray
         Array of measured energies to be converted to true energies
-    fitparams: list
+    fitparams : list
         List containing the best fit parameters from the fit_saturation() 
         fuction. fitparams[0] should correspond to the optimum parameters
         and fitparams[1] should be the covariance matrix from the fit
         
     Returns
     -------
-    energy_true: array
+    energy_true : ndarray
         Array of saturation corrected energies
-    errors: array
+    errors : ndarray
         Array of uncertainties for each value in energy_true
         
     """
