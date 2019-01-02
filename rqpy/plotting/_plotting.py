764c23b81d4bf08626678738729245526a7e19cb
Samuel Watkins <35979561+slwatkins@users.noreply.github.com>
2019-01-02 15:17:32 -0800

    hist() and scatter() now support arbitrary numbers of cuts for plotting (#26)
    * changed hist to support plotting any number of cuts
    
    * scatter takes an arbitrary number of cuts, updated hist documentation

--- rqpy/plotting/_plotting.py
+++ rqpy/plotting/_plotting.py
@@ -8,11 +8,10 @@ from rqpy import utils
 __all__ = ["hist", "scatter", "densityplot", "plot_gauss", "plot_n_gauss", "plot_saturation_correction"]
 
 
-def hist(arr, nbins='sqrt', xlims=None, cutold=None, cutnew=None, lgcrawdata=True, 
-         lgceff=True, lgclegend=True, labeldict=None, ax=None):
+def hist(arr, nbins='sqrt', xlims=None, cuts=None, lgcrawdata=True, 
+         lgceff=True, lgclegend=True, labeldict=None, ax=None, cmap="viridis"):
     """
-    Function to plot histogram of RQ data. The bins are set such that all bins have the same size
-    as the raw data
+    Function to plot histogram of RQ data with multiple cuts.
     
     Parameters
     ----------
@@ -22,25 +21,28 @@ def hist(arr, nbins='sqrt', xlims=None, cutold=None, cutnew=None, lgcrawdata=Tru
         This is the same as plt.hist() bins parameter. Defaults is 'sqrt'.
     xlims : list of float, optional
         The xlimits of the histogram. This is passed to plt.hist() range parameter.
-    cutold : array of bool, optional
-        Mask of values to be plotted
-    cutnew : array of bool, optional
-        Mask of values to be plotted. This mask is added to cutold if cutold is not None. 
+    cuts : list, optional
+        List of masks of values to be plotted. The cuts will be applied in the order that they are listed, 
+        such that any number of cuts can be plotted
     lgcrawdata : bool, optional
         If True, the raw data is plotted
     lgceff : bool, optional
-        If True, the cut efficiencies are printed in the legend. The total eff will be the sum of all the 
-        cuts divided by the length of the data. The current cut eff will be the sum of the current cut 
-        divided by the sum of all the previous cuts, if any.
+        If True, the cut efficiencies are printed in the legend. 
     lgclegend : bool, optional
         If True, the legend is plotted.
     labeldict : dict, optional
-        Dictionary to overwrite the labels of the plot. defaults are : 
-            labels = {'title' : 'Histogram', 'xlabel' : 'variable', 'ylabel' : 'Count', 
-            'cutnew' : 'current', 'cutold' : 'previous'}
-        Ex: to change just the title, pass: labeldict = {'title' : 'new title'}, to histrq()
+        Dictionary to overwrite the labels of the plot. defaults are: 
+            labels = {'title' : 'Histogram', 
+                      'xlabel' : 'variable', 
+                      'ylabel' : 'Count', 
+                      'cut0' : '1st', 
+                      'cut1' : '2nd', 
+                      ...}
+        Ex: to change just the title, pass: labeldict = {'title' : 'new title'}, to hist()
     ax : axes.Axes object, optional
         Option to pass an existing Matplotlib Axes object to plot over, if it already exists.
+    cmap : str, optional
+        The colormap to use for plotting each cut. Default is 'viridis'.
     
     Returns
     -------
@@ -51,15 +53,31 @@ def hist(arr, nbins='sqrt', xlims=None, cutold=None, cutnew=None, lgcrawdata=Tru
         
     """
     
+    if not isinstance(cuts, list):
+        cuts = [cuts]
+    
     labels = {'title'  : 'Histogram', 
               'xlabel' : 'variable', 
-              'ylabel' : 'Count', 
-              'cutnew' : 'current', 
-              'cutold' : 'previous'}
+              'ylabel' : 'Count'}
     
+    
+    for ii in range(len(cuts)):
+        
+        num_str = str(ii+1)
+        
+        if num_str[-1]=='1':
+            num_str+="st"
+        elif num_str[-1]=='2':
+            num_str+="nd"
+        elif num_str[-1]=='3':
+            num_str+="rd"
+        else:
+            num_str+="th"
+        
+        labels[f"cut{ii}"] = num_str
+        
     if labeldict is not None:
-        for key in labeldict:
-            labels[key] = labeldict[key]
+        labels.update(labeldict)
     
     if ax is None:
         fig, ax = plt.subplots(figsize=(9, 6))
@@ -73,68 +91,47 @@ def hist(arr, nbins='sqrt', xlims=None, cutold=None, cutnew=None, lgcrawdata=Tru
     if lgcrawdata:
         if xlims is None:
             hist, bins, _ = ax.hist(arr, bins=nbins, histtype='step', 
-                                    label='full data', linewidth=2, color='b')
+                                    label='Full data', linewidth=2, color=plt.cm.get_cmap(cmap)(0))
             xlims = (bins.min(), bins.max())
         else:
             hist, bins, _ = ax.hist(arr, bins=nbins, range=xlims, histtype='step', 
-                                    label='full data', linewidth=2, color='b')            
-    if cutold is not None:
-        oldsum = cutold.sum()
-        if cutnew is None:
-            cuteff = oldsum/cutold.shape[0]
-            cutefftot = cuteff
-        if lgcrawdata:
-            nbins = bins
-        label = f"Data passing {labels['cutold']} cut"
-        if xlims is not None:
-            ax.hist(arr[cutold], bins=nbins, range=xlims, histtype='step', 
-                    label=label, linewidth=2, color='r')
-        else:
-            res = ax.hist(arr[cutold], bins=nbins, histtype='step', 
-                    label=label, linewidth=2, color='r')
-            xlims = (res[1].min(), res[1].max())
-    if cutnew is not None:
-        newsum = cutnew.sum()
-        if cutold is not None:
-            cutnew = cutnew & cutold
-            cuteff = cutnew.sum()/oldsum
-            cutefftot = cutnew.sum()/cutnew.shape[0]
-        else:
-            cuteff = newsum/cutnew.shape[0]
-            cutefftot = cuteff
-        if lgcrawdata:
-            nbins = bins
-        if lgceff:
-            label = f"Data passing {labels['cutnew']} cut, eff :  {cuteff:.3f}"
-        else:
-            label = f"Data passing {labels['cutnew']} cut "
-        if xlims is not None:
-            ax.hist(arr[cutnew], bins=nbins, range=xlims, histtype='step', 
-                    linewidth=2, color='g', label=label)
-        else:
-            res = ax.hist(arr[cutnew], bins=nbins, histtype='step', 
-                    linewidth=2, color='g', label=label)
-            xlims = (res[1].min(), res[1].max())
-    elif (cutnew is None) & (cutold is None):
-        cuteff = 1
-        cutefftot = 1
+                                    label='Full data', linewidth=2, color=plt.cm.get_cmap(cmap)(0))
+            
+    colors = plt.cm.get_cmap(cmap)(np.linspace(0.1, 0.9, len(cuts)))
         
+    ctemp = np.ones(len(arr), dtype=bool)
+    
+    for ii, cut in enumerate(cuts):
+        oldsum = ctemp.sum()
+        ctemp = ctemp & cut
+        newsum = ctemp.sum()
+        cuteff = newsum/oldsum * 100
+        label = f"Data passing {labels[f'cut{ii}']} cut"
+        
+        if lgceff:
+            label+=f", Eff = {cuteff:.1f}%"
+            
+        if xlims is not None:
+            ax.hist(arr[ctemp], bins=nbins, range=xlims, histtype='step', 
+                    label=label, linewidth=2, color=colors[ii])
+        else:
+            res = ax.hist(arr[ctemp], bins=nbins, histtype='step', 
+                    label=label, linewidth=2, color=colors[ii])
+            xlims = (res[1].min(), res[1].max())
+    
     ax.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
     ax.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
     ax.tick_params(which="both", direction="in", right=True, top=True)
     ax.grid(linestyle="dashed")
     
-    if lgceff:
-        ax.plot([], [], linestyle=' ', label=f'Efficiency of total cut: {cutefftot:.3f}')
     if lgclegend:
-        ax.legend()
+        ax.legend(loc="best")
+        
     return fig, ax
     
 
-
-
-def scatter(xvals, yvals, xlims=None, ylims=None, cutold=None, cutnew=None, 
-            lgcrawdata=True, lgceff=True, lgclegend=True, labeldict=None, ms=1, a=.3, ax=None):
+def scatter(xvals, yvals, xlims=None, ylims=None, cuts=None, lgcrawdata=True, lgceff=True, 
+            lgclegend=True, labeldict=None, ms=1, a=.3, ax=None, cmap="viridis"):
     """
     Function to plot RQ data as a scatter plot.
     
@@ -150,29 +147,33 @@ def scatter(xvals, yvals, xlims=None, ylims=None, cutold=None, cutnew=None,
     ylims : list of float, optional
         This is passed to the plot as the y limits. Automatically determined from range of data
         if not set.
-    cutold : array of bool, optional
-        Mask of values to be plotted
-    cutnew : array of bool, optional
-        Mask of values to be plotted. This mask is added to cutold if cutold is not None. 
+    cuts : list, optional
+        List of masks of values to be plotted. The cuts will be applied in the order that they are listed,
+        such that any number of cuts can be plotted
     lgcrawdata : bool, optional
         If True, the raw data is plotted
     lgceff : bool, optional
-        If True, the cut efficiencies are printed in the legend. The total eff will be the sum of all the 
-        cuts divided by the length of the data. The current cut eff will be the sum of the current cut 
-        divided by the sum of all the previous cuts, if any.
+        If True, the efficiencies of each cut, with respect to the data that survived 
+        the previous cut, are printed in the legend. 
     lgclegend : bool, optional
-        If True, the legend is plotted.
+        If True, the legend is included in the plot.
     labeldict : dict, optional
-        Dictionary to overwrite the labels of the plot. defaults are : 
-            labels = {'title' : 'Histogram', 'xlabel' : 'variable', 'ylabel' : 'Count', 
-            'cutnew' : 'current', 'cutold' : 'previous'}
-        Ex: to change just the title, pass: labeldict = {'title' : 'new title'}, to histrq()
+        Dictionary to overwrite the labels of the plot. defaults are: 
+            labels = {'title' : 'Scatter Plot', 
+                      'xlabel' : 'x variable', 
+                      'ylabel' : 'y variable', 
+                      'cut0' : '1st', 
+                      'cut1' : '2nd', 
+                      ...}
+        Ex: to change just the title, pass: labeldict = {'title' : 'new title'}, to scatter()
     ms : float, optional
         The size of each marker in the scatter plot. Default is 1
     a : float, optional
         The opacity of the markers in the scatter plot, i.e. alpha. Default is 0.3
     ax : axes.Axes object, optional
         Option to pass an existing Matplotlib Axes object to plot over, if it already exists.
+    cmap : str, optional
+        The colormap to use for plotting each cut. Default is 'viridis'.
     
     Returns
     -------
@@ -183,15 +184,30 @@ def scatter(xvals, yvals, xlims=None, ylims=None, cutold=None, cutnew=None,
         
     """
 
+    if not isinstance(cuts, list):
+        cuts = [cuts]
+    
     labels = {'title'  : 'Scatter Plot',
               'xlabel' : 'x variable', 
-              'ylabel' : 'y variable', 
-              'cutnew' : 'current', 
-              'cutold' : 'previous'}
+              'ylabel' : 'y variable'}
     
+    for ii in range(len(cuts)):
+        
+        num_str = str(ii+1)
+        
+        if num_str[-1]=='1':
+            num_str+="st"
+        elif num_str[-1]=='2':
+            num_str+="nd"
+        elif num_str[-1]=='3':
+            num_str+="rd"
+        else:
+            num_str+="th"
+        
+        labels[f"cut{ii}"] = num_str
+        
     if labeldict is not None:
-        for key in labeldict:
-            labels[key] = labeldict[key]
+        labels.update(labeldict)
     
     if ax is None:
         fig, ax = plt.subplots(figsize=(9, 6))
@@ -206,83 +222,60 @@ def scatter(xvals, yvals, xlims=None, ylims=None, cutold=None, cutnew=None,
         xlimitcut = (xvals>xlims[0]) & (xvals<xlims[1])
     else:
         xlimitcut = np.ones(len(xvals), dtype=bool)
+        
     if ylims is not None:
         ylimitcut = (yvals>ylims[0]) & (yvals<ylims[1])
     else:
         ylimitcut = np.ones(len(yvals), dtype=bool)
-
+    
     limitcut = xlimitcut & ylimitcut
     
-    if lgcrawdata and cutold is not None: 
-        ax.scatter(xvals[limitcut & ~cutold], yvals[limitcut & ~cutold], 
-                   label='Full Data', c='b', s=ms, alpha=a)
-    elif lgcrawdata and cutnew is not None: 
-        ax.scatter(xvals[limitcut & ~cutnew], yvals[limitcut & ~cutnew], 
+    if lgcrawdata and cuts is not None: 
+        ax.scatter(xvals[limitcut & ~cuts[0]], yvals[limitcut & ~cuts[0]], 
                    label='Full Data', c='b', s=ms, alpha=a)
     elif lgcrawdata:
         ax.scatter(xvals[limitcut], yvals[limitcut], 
                    label='Full Data', c='b', s=ms, alpha=a)
-        
-    if cutold is not None:
-        oldsum = cutold.sum()
-        if cutnew is None:
-            cuteff = cutold.sum()/cutold.shape[0]
-            cutefftot = cuteff
-            
-        label = f"Data passing {labels['cutold']} cut"
-        if cutnew is None:
-            ax.scatter(xvals[cutold & limitcut], yvals[cutold & limitcut], 
-                       label=label, c='r', s=ms, alpha=a)
-        else: 
-            ax.scatter(xvals[cutold & limitcut & ~cutnew], yvals[cutold & limitcut & ~cutnew], 
-                       label=label, c='r', s=ms, alpha=a)
-        
-    if cutnew is not None:
-        newsum = cutnew.sum()
-        if cutold is not None:
-            cutnew = cutnew & cutold
-            cuteff = cutnew.sum()/oldsum
-            cutefftot = cutnew.sum()/cutnew.shape[0]
-        else:
-            cuteff = newsum/cutnew.shape[0]
-            cutefftot = cuteff
-            
-        if lgceff:
-            label = f"Data passing {labels['cutnew']} cut, eff : {cuteff:.3f}"
-        else:
-            label = f"Data passing {labels['cutnew']} cut"
-            
-        ax.scatter(xvals[cutnew & limitcut], yvals[cutnew & limitcut], 
-                   label=label, c='g', s=ms, alpha=a)
     
-    elif (cutnew is None) & (cutold is None):
-        cuteff = 1
-        cutefftot = 1
+    colors = plt.cm.get_cmap(cmap)(np.linspace(0.1, 0.9, len(cuts)))
+        
+    ctemp = np.ones(len(xvals), dtype=bool)
+    
+    for ii, cut in enumerate(cuts):
+        oldsum = ctemp.sum()
+        ctemp = ctemp & cut
+        newsum = ctemp.sum()
+        cuteff = newsum/oldsum * 100
+        label = f"Data passing {labels[f'cut{ii}']} cut"
+        
+        if lgceff:
+            label+=f", Eff = {cuteff:.1f}%"
+            
+        cplot = ctemp & limitcut
+        
+        if ii+1<len(cuts):
+            cplot = cplot & ~cuts[ii+1]
+        
+        ax.scatter(xvals[cplot], yvals[cplot], 
+                   label=label, c=colors[ii], s=ms, alpha=a)
         
     if xlims is None:
-        if lgcrawdata:
+        if lgcrawdata and cuts is None:
             xrange = xvals.max()-xvals.min()
             ax.set_xlim([xvals.min()-0.05*xrange, xvals.max()+0.05*xrange])
-        elif cutold is not None:
-            xrange = xvals[cutold].max()-xvals[cutold].min()
-            ax.set_xlim([xvals[cutold].min()-0.05*xrange, xvals[cutold].max()+0.05*xrange])
-        elif cutnew is not None:
-            xrange = xvals[cutnew].max()-xvals[cutnew].min()
-            ax.set_xlim([xvals[cutnew].min()-0.05*xrange, xvals[cutnew].max()+0.05*xrange])
+        elif cuts is not None:
+            xrange = xvals[cuts[0]].max()-xvals[cuts[0]].min()
+            ax.set_xlim([xvals[cuts[0]].min()-0.05*xrange, xvals[cuts[0]].max()+0.05*xrange])
     else:
         ax.set_xlim(xlims)
         
     if ylims is None:
-        if lgcrawdata:
+        if lgcrawdata and cuts is None:
             yrange = yvals.max()-yvals.min()
             ax.set_ylim([yvals.min()-0.05*yrange, yvals.max()+0.05*yrange])
-        elif cutold is not None:
-            yrange = yvals[cutold].max()-yvals[cutold].min()
-            ax.set_ylim([yvals[cutold].min()-0.05*yrange, yvals[cutold].max()+0.05*yrange])
-        elif cutnew is not None:
-            yrange = yvals[cutnew].max()-yvals[cutnew].min()
-            ax.set_ylim([yvals[cutnew].min()-0.05*yrange, yvals[cutnew].max()+0.05*yrange])
-        
+        elif cuts is not None:
+            yrange = yvals[cuts[0]].max()-yvals[cuts[0]].min()
+            ax.set_ylim([yvals[cuts[0]].min()-0.05*yrange, yvals[cuts[0]].max()+0.05*yrange])
     else:
         ax.set_ylim(ylims)
         
@@ -291,8 +284,6 @@ def scatter(xvals, yvals, xlims=None, ylims=None, cutold=None, cutnew=None,
     ax.tick_params(which="both", direction="in", right=True, top=True)
     ax.grid(linestyle="dashed")
     
-    if lgceff:
-        ax.plot([], [], linestyle=' ', label=f'Efficiency of total cut: {cutefftot:.3f}')
     if lgclegend:
         ax.legend(markerscale=6, framealpha=.9)
     
