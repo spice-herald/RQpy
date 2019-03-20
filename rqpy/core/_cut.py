3d99b6e455f48e566388fec951f430c8aebbc095
Samuel Watkins <samwatkins@galba.stanford.edu>
2019-03-20 10:48:05 -0700

    add uncertainty information to the passage fraction calculation

--- rqpy/core/_cut.py
+++ rqpy/core/_cut.py
@@ -308,7 +308,7 @@ def passage_fraction(x, cut, basecut=None, nbins=100, lgcequaldensitybins=False)
     """
     Function for returning the passage fraction of a cut as a function of the specified
     variable `x`.
-    
+
     Parameters
     ----------
     x : array_like
@@ -323,30 +323,60 @@ def passage_fraction(x, cut, basecut=None, nbins=100, lgcequaldensitybins=False)
     lgcequaldensitybins : bool, optional
         If set to True, the bin widths are set such that each bin has the same number
         of data points within it. If left as False, then a constant bin width is used.
-    
+
     Returns
     -------
     x_binned : ndarray
         The corresponding `x` values for each passage fraction, given as the edges of each bin.
-    frac_binned : ndarray
-        The passage fractions for each value of `x_binned` for the given `cut` and `basecut`.
-    
+    frac_binned_mp : ndarray
+        The most probable (measured) passage fraction for each value of `x_binned` for the given
+        `cut` and `basecut`.
+    frac_binned_biased : ndarray
+        The expected value of the passage fraction for each value of `x_binned` for the given
+        `cut` and `basecut`. See Notes for more information.
+    frac_binned_err : ndarray
+        The standard deviation of the passage fraction for each value of `x_binned` for the
+        given `cut` and `basecut`, where this is centered on the values of `frac_binned_biased`.
+        See Notes for more information.
+
+    Notes
+    -----
+    The errors are based on the derivation in https://arxiv.org/pdf/physics/0701199v1.pdf.
+
+    Let \eps be the passage fraction. Then we have that:
+
+    P(\eps | n, k) = (n + 1)! / (k! (n - k)!) * \eps^k * (1 - \eps)^(n - k)
+
+    E[\eps] = Integral(\eps * P(\eps | n, k), 0, 1) = (k + 1) / (n + 2)
+
+    This is a biased estimator of the efficiency. An unbiased estimator is the solution 
+    to dP/d\eps = 0, which is \eps_{mp} = k / n (the same as the measured passage fraction).
+
+    The variance is given by:
+
+    V[\eps] = E[\eps^2] - E[\eps]^2 = (k + 1) / (n + 2) * ( (k + 2) / (n + 3) - (k + 1) / (n + 2))
+
+    Then the standard deviation is \sigma_{\eps} = \sqrt{V[\eps]}.
+
     """
-    
+
     if basecut is None:
         basecut = np.ones(len(x), dtype=bool)
-    
+
     if lgcequaldensitybins:
         histbins_equal = lambda var, nbin: np.interp(np.linspace(0, len(var), nbin + 1),
                                                      np.arange(len(var)),
                                                      np.sort(var))
         nbins = histbins_equal(x[basecut], nbins)
-    
+
     hist_vals_base, x_binned = np.histogram(x[basecut], bins=nbins)
     hist_vals, _ = np.histogram(x[basecut & cut], bins=x_binned)
-    
+
+    frac_binned_biased = (hist_vals + 1)/(hist_vals_base + 2)
+    frac_binned_err = np.sqrt(frac_binned_biased * ((hist_vals + 2)/(hist_vals_base + 3) - frac_binned_biased))
+
     hist_vals_base[hist_vals_base==0] = 1
-    
-    frac_binned = hist_vals/hist_vals_base
-    
-    return x_binned, frac_binned
+
+    frac_binned_mp = hist_vals/hist_vals_base
+
+    return x_binned, frac_binned_mp, frac_binned_biased, frac_binned_err
