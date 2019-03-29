51291cbb77e0543b4e013e8ecab52735afe4ea9f
Samuel Watkins <samwatkins@galba.stanford.edu>
2019-03-29 11:13:59 -0700

    change setup to compile fortran code

--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
 import os
 import glob
 import shutil
-from setuptools import setup, find_packages, Command
+from setuptools import find_packages, Command
 
+from numpy.distutils.core import Extension, setup
+
+
+upperlim_files = ['upperlim.pyf',
+                  'UpperLim.f',
+                  'y_vs_CLf.f',
+                  'CMaxinf.f',
+                  'ConfLev.f',
+                  'Cinf.f',
+                  'CERN_Stuff.f',
+                 ]
+
+f77_paths = []
+for fname in upperlim_files:
+    f77_paths.append(f"rqpy/limit/_upperlim/{fname}")
+
+ext1 = Extension(name='rqpy.limit._upperlim.upperlim',
+                 sources=f77_paths,
+                )
 
 class CleanCommand(Command):
     """Custom clean command to tidy up the project root."""
@@ -40,5 +59,8 @@ setup(
     zip_safe=False,
     cmdclass={
             'clean': CleanCommand,
-            }
-    )
+            },
+    package_data = {'': ['*.f', '*.pyf']}, 
+    include_package_data = True,
+    ext_modules=[ext1],
+)
