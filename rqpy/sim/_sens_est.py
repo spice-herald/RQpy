9e68f502184b21b7eafd195a134518a7f0ce49fb
Samuel Watkins <samwatkins@berkeley.edu>
2020-06-18 00:52:31 +0000

    white space fixes, remove return of entire list of cross sections

--- rqpy/sim/_sens_est.py
+++ rqpy/sim/_sens_est.py
@@ -15,24 +15,30 @@ __all__ = [
 
 def calculate_substrate_mass(vol, tm):
     """
-    Helper function for calculating the mass of substrate given its volume.
+    Helper function for calculating the mass of substrate given its
+    volume.
 
     Parameters
     ----------
     vol : float
-        The volume of the substrate that we want to know the mass of, in units of meters^3.
+        The volume of the substrate that we want to know the mass of,
+        in units of meters^3.
     tm : str, int
-        The target material of the detector. Can be passed as either the atomic symbol, the
-        atomic number, or the full name of the element. Default is 'Si'.
+        The target material of the detector. Can be passed as either
+        the atomic symbol, the atomic number, or the full name of the
+        element. Default is 'Si'.
 
     Returns
     -------
     mass : float
-        The mass, in kg, of the substrate based on the inputted volume and target material.
+        The mass, in kg, of the substrate based on the inputted volume
+        and target material.
 
     """
 
-    rho = mendeleev.element(tm).density / (rp.constants.centi**3 * rp.constants.kilo) # density in kg/m^3
+    conv_factor = rp.constants.centi**3 * rp.constants.kilo
+    # density in kg/m^3
+    rho = mendeleev.element(tm).density / conv_factor
     mass = rho * vol
 
     return mass
@@ -40,20 +46,22 @@ def calculate_substrate_mass(vol, tm):
 
 def trigger_pdf(x, sigma, n_win):
     """
-    Function for calculating the expected PDF due to a finite trigger window, based
-    on the Optimum Filter method. Outputs in units of 1 / [units of `sigma`].
+    Function for calculating the expected PDF due to a finite trigger
+    window, based on the Optimum Filter method. Outputs in units of
+    1 / [units of `sigma`].
 
     Parameters
     ----------
     x : ndarray
-        The values at which the PDF will be evaluated. Units are arbitrary,
-        but note that this function is usually used with regards to energy.
+        The values at which the PDF will be evaluated. Units are
+        arbitrary, but note that this function is usually used with
+        regards to energy.
     sigma : float
         The detector resolution in the same units of `x`.
     n_win : float
-        The number of independent samples in a trigger search window. If there
-        is correlated noise, this value can be shorter than the number of samples
-        in the trigger window.
+        The number of independent samples in a trigger search window.
+        If there is correlated noise, this value can be shorter than
+        the number of samples in the trigger window.
 
     Returns
     -------
@@ -72,15 +80,17 @@ def trigger_pdf(x, sigma, n_win):
 
 class SensEst(object):
     """
-    Class for setting up and running an estimate of the sensitivity of a device, given expected backgrounds.
+    Class for setting up and running an estimate of the sensitivity of
+    a device, given expected backgrounds.
 
     Attributes
     ----------
     m_det : float
         Mass of the detector in kg.
     tm : str, int
-        The target material of the detector. Can be passed as either the atomic symbol, the
-        atomic number, or the full name of the element.
+        The target material of the detector. Can be passed as either
+        the atomic symbol, the atomic number, or the full name of the
+        element.
     exposure : float
         The total exposure of the detector in units of kg*days.
 
@@ -97,10 +107,12 @@ class SensEst(object):
         time_elapsed : float
             The time elapsed for the simulated experiment, in days.
         eff : float, optional
-            The estimated efficiency due to data selection, live time losses, etc. Default is 1.
+            The estimated efficiency due to data selection, live time
+            losses, etc. Default is 1.
         tm : str, int, optional
-            The target material of the detector. Can be passed as either the atomic symbol, the
-            atomic number, or the full name of the element. Default is 'Si'.
+            The target material of the detector. Can be passed as
+            either the atomic symbol, the atomic number, or the full
+            name of the element. Default is 'Si'.
 
         """
 
@@ -117,7 +129,8 @@ class SensEst(object):
         Parameters
         ----------
         flat_rate : float
-            The flat background rate, in units of events/kg/kev/day (DRU).
+            The flat background rate, in units of events/kg/kev/day
+            (DRU).
 
         """
 
@@ -134,12 +147,12 @@ class SensEst(object):
         sigma : float
             The detector resolution in units of keV.
         n_win : float
-            The number of independent samples in a trigger search window. If there
-            is correlated noise, this value can be shorter than the number of samples
-            in the trigger window.
+            The number of independent samples in a trigger search
+            window. If there is correlated noise, this value can be
+            shorter than the number of samples in the trigger window.
         fs : float
-            The digitization rate of the data being used in the trigger algorithm,
-            units of Hz.
+            The digitization rate of the data being used in the trigger
+            algorithm, units of Hz.
 
         """
 
@@ -155,11 +168,11 @@ class SensEst(object):
         Parameters
         ----------
         m_dm : float
-            The dark matter mass at which to calculate the expected differential
-            event rate. Expected units are GeV/c^2.
+            The dark matter mass at which to calculate the expected
+            differential event rate. Expected units are GeV/c^2.
         sig0 : float
-            The dark matter cross section at which to calculate the expected
-            differential event rate. Expected units are cm^2.
+            The dark matter cross section at which to calculate the
+            expected differential event rate. Expected units are cm^2.
 
         """
 
@@ -174,8 +187,9 @@ class SensEst(object):
         Parameters
         ----------
         function : FunctionType
-            A function that returns a background rate in units of events/kg/keV/day
-            when inputted energy, where the energies are in units of keV.
+            A function that returns a background rate in units of
+            events/kg/keV/day when inputted energy, where the energies
+            are in units of keV.
 
         """
 
@@ -188,36 +202,43 @@ class SensEst(object):
         self._backgrounds = []
 
 
-    def run_sim(self, threshold, e_high, m_dms=None, nexp=1, npts=1000, plot_bkgd=False):
+    def run_sim(self, threshold, e_high, m_dms=None, nexp=1, npts=1000,
+                plot_bkgd=False):
         """
-        Method for running the simulation for getting the sensitivity estimate.
+        Method for running the simulation for getting the sensitivity
+        estimate.
 
         Parameters
         ----------
         threshold : float
             The energy threshold of the experiment, units of keV.
         e_high : float
-            The high energy cutoff of the analysis range, as we need some cutoff to the
-            event energies that we generate.
+            The high energy cutoff of the analysis range, as we need
+            some cutoff to the event energies that we generate.
         m_dms : ndarray, optional
-            Array of dark matter masses (in GeV/c^2) to run the Optimum Interval code.
-            Default is 50 points from 0.05 to 2 GeV/c^2.
+            Array of dark matter masses (in GeV/c^2) to run the Optimum
+            Interval code. Default is 50 points from 0.05 to 2 GeV/c^2.
         nexp : int, optional
-            The number of experiments to run - the median of the outputs will be taken.
-            Recommended to set to 1 for diagnostics, which is default.
+            The number of experiments to run - the median of the
+            outputs will be taken. Recommended to set to 1 for
+            diagnostics, which is default.
         npts : int, optional
-            The number of points to use when interpolating the simulated background rates.
-            Default is 1000.
+            The number of points to use when interpolating the
+            simulated background rates. Default is 1000.
         plot_bkgd : bool, optional
-            Option to plot the background being used on top of the generated data, for diagnostic
-            purposes. If `nexp` is greater than 1, then only the first generated dataset is plotted.
+            Option to plot the background being used on top of the
+            generated data, for diagnostic purposes. If `nexp` is
+            greater than 1, then only the first generated dataset is
+            plotted.
 
         Returns
         -------
         m_dms : ndarray
-            The dark matter masses in GeV/c^2 that upper limit was set at.
+            The dark matter masses in GeV/c^2 that upper limit was set
+            at.
         sig : ndarray
-            The cross section in cm^2 that the upper limit was determined to be.
+            The cross section in cm^2 that the upper limit was
+            determined to be.
 
         """
 
@@ -252,35 +273,40 @@ class SensEst(object):
 
         sig = np.median(np.stack(sigs, axis=1), axis=1)
 
-        return m_dms, sig, sigs
+        return m_dms, sig
 
-    def generate_background(self, e_high, e_low=0, npts=1000, plot_bkgd=False):
+    def generate_background(self, e_high, e_low=0, npts=1000,
+                            plot_bkgd=False):
         """
         Method for generating events based on the inputted background.
 
         Parameters
         ----------
         e_high : float
-            The high energy cutoff of the analysis range, as we need some cutoff to the
-            event energies that we generate.
+            The high energy cutoff of the analysis range, as we need
+            some cutoff to the event energies that we generate.
         e_low : float, optional
             The low energy cutoff of the analysis range, default is 0.
         npts : int, optional
-            The number of points to use when interpolating the simulated background rates.
-            Default is 1000.
+            The number of points to use when interpolating the
+            simulated background rates. Default is 1000.
         plot_bkgd : bool, optional
-            Option to plot the background being used on top of the generated data, for diagnostic
-            purposes. If `nexp` is greater than 1, then only the first generated dataset is plotted.
+            Option to plot the background being used on top of the
+            generated data, for diagnostic purposes. If `nexp` is
+            greater than 1, then only the first generated dataset is
+            plotted.
 
         Returns
         -------
         evts_sim : ndarray
-            The array of all the simulated events based on the inputted backgrounds. Units are keV.
+            The array of all the simulated events based on the inputted
+            backgrounds. Units are keV.
 
         Raises
         ------
         ValueError
-            If `self._backgrounds` is an empty list (no backgrounds have been added)
+            If `self._backgrounds` is an empty list (no backgrounds
+            have been added).
 
         """
 
@@ -291,32 +317,38 @@ class SensEst(object):
 
     def _generate_background(self, en_interp, plot_bkgd=False):
         """
-        Hidden method for generating events based on the inputted background.
+        Hidden method for generating events based on the inputted
+        background.
 
         Parameters
         ----------
         en_interp : ndarray
-            The energies at which the total simulated background rate will be interpolated,
-            in units of keV.
+            The energies at which the total simulated background rate
+            will be interpolated, in units of keV.
         plot_bkgd : bool, optional
-            Option to plot the background being used on top of the generated data, for diagnostic
-            purposes. If `nexp` is greater than 1, then only the first generated dataset is plotted.
+            Option to plot the background being used on top of the
+            generated data, for diagnostic purposes. If `nexp` is
+            greater than 1, then only the first generated dataset is
+            plotted.
 
         Returns
         -------
         evts_sim : ndarray
-            The array of all the simulated events based on the inputted backgrounds. Units are keV.
+            The array of all the simulated events based on the inputted
+            backgrounds. Units are keV.
 
         Raises
         ------
         ValueError
-            If `self._backgrounds` is an empty list (no backgrounds have been added)
+            If `self._backgrounds` is an empty list (no backgrounds
+            have been added).
 
         """
 
         if len(self._backgrounds) == 0:
             raise ValueError(
-                "No backgrounds have been added, add some using the methods of SensEst."
+                "No backgrounds have been added, "
+                "add some using the methods of SensEst."
             )
 
         e_high = en_interp.max()
@@ -327,7 +359,9 @@ class SensEst(object):
         for bkgd in self._backgrounds:
             tot_bkgd += bkgd(en_interp)
 
-        tot_bkgd_func = lambda x: np.stack([bkgd(x) for bkgd in self._backgrounds], axis=1).sum(axis=1)
+        tot_bkgd_func = lambda x: np.stack(
+            [bkgd(x) for bkgd in self._backgrounds], axis=1,
+        ).sum(axis=1)
 
         rtot = np.trapz(tot_bkgd_func(en_interp), x=en_interp)
 
@@ -344,9 +378,15 @@ class SensEst(object):
         return evts_sim
 
     def _plot_bkgd(self, evts, en_interp, tot_bkgd_func):
-        """Hidden Method for plotting the generated events on top of the inputted backgrounds."""
+        """
+        Hidden Method for plotting the generated events on top of the
+        inputted backgrounds.
 
-        ratecomp = rp.RatePlot((en_interp.min(), en_interp.max()), figsize=(9, 6))
+        """
+
+        ratecomp = rp.RatePlot(
+            (en_interp.min(), en_interp.max()), figsize=(9, 6),
+        )
         ratecomp.add_data(
             evts,
             self.exposure,
@@ -379,7 +419,13 @@ class SensEst(object):
         )
 
         ratecomp.ax.legend(fontsize=14)
-        for item in [ratecomp.ax.title, ratecomp.ax.xaxis.label, ratecomp.ax.yaxis.label]:
+        list_of_text = [
+            ratecomp.ax.title,
+            ratecomp.ax.xaxis.label,
+            ratecomp.ax.yaxis.label,
+        ]
+
+        for item in list_of_text:
             item.set_fontsize(14)
 
         ratecomp.fig.tight_layout()
