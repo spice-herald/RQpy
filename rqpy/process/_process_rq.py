bb283896e74fcacdb9dbe5700b24e032d798da45
Samuel Watkins <samwatkins@berkeley.edu>
2020-06-19 00:09:05 +0000

    ofamp_constrained defaults to ofamp_constrained_windowcenter if trigsim hasn't been run

--- rqpy/process/_process_rq.py
+++ rqpy/process/_process_rq.py
@@ -1585,7 +1585,7 @@ def _calc_rq_single_channel(signal, template, psd, setup, readout_inds, chan, ch
                     triggertime_sim_constrained[jj] = res_trigsim_constrained[1]
 
             if setup.do_ofamp_constrained[chan_num]:
-                if setup.ofamp_constrained_usetrigsimcenter[chan_num]:
+                if setup.ofamp_constrained_usetrigsimcenter[chan_num] and (setup.do_trigsim[chan_num] and setup.trigger == chan_num):
                     windowcenter_constrain = int(triggertime_sim[jj] * setup.fs - (signal.shape[-1]//2))
                     if setup.indstart is not None:
                         windowcenter_constrain -= setup.indstart
